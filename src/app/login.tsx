fec9bf5b35321661817a25c86b3905ed7999ee2e
Mayara <maya.spieker@gmail.com>
2025-01-15 16:52:38 -0300

    fix: login e toast

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,30 +1,39 @@
 import { Input } from "@/components/input";
 import { Image, StyleSheet, View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
-import { colors } from "@/utils/globals";
-import { stylesInit } from "./signup-company";
+import { stylesInit } from "./company-register/step-one";
 import { PrimaryButton } from "@/components/primary-button";
 import { useContext, useState } from "react";
 import { StatusCode } from "api/client/IHttpClient";
-import { MessageToast } from "@/components/message-toast";
 import { AuthContext } from "api/context/auth";
 import { router } from "expo-router";
 import { useForm } from "react-hook-form";
+import Toast from "react-native-toast-message";
 
 export default function Login() {
     const { control } = useForm();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
     const auth = useContext(AuthContext)
 
     async function onSubmit() {
         const response = await auth.login({ password, email })
 
         if (response.statusCode === StatusCode.NotFound) {
-            setError(response.reject)
+            Toast.show({
+                type: 'error',
+                text1: 'Erro',
+                text2: 'Usuário não encontrado.',
+                autoHide: true,
+                visibilityTime: 2000
+            })
         } else if (response.statusCode === StatusCode.Ok) {
-            setSuccess(response.resolve)
+            Toast.show({
+                type: 'success',
+                text1: 'Sucesso',
+                text2: 'Usuário logado com sucesso.',
+                autoHide: true,
+                visibilityTime: 2000
+            })
             setTimeout(() => {
                 router.replace('/profile')
             }, 2000)
@@ -37,36 +46,27 @@ export default function Login() {
                 <View style={styles.componentView}>
                     <View style={styles.logoView}>
                         <Image style={styles.image} source={require('../../assets/images/logo-w-name.png')} />
-                        <Text style={stylesInit.text}>Entre para utilizar a plataforma</Text>
+                        <Text style={stylesInit.text}>Entre para utilizar a plataforma.</Text>
                     </View>
                     <View style={styles.inputsView}>
                         <Input
                             icon='mail'
-                            inputProps={{ placeholder: 'Digite seu e-mail', onChangeText: setEmail, keyboardType: 'email-address' }}
-                            formProps={{ name: 'email', control }}
-                            error={error}
-                        // onChangeText={(email) => setEmail(email)} 
-                        // label='Email' 
-                        // type='email' 
-                        // placeholder='Digite seu email' 
-                        // color={colors.white}
-                        />
+                            inputProps={{
+                                placeholder: 'Digite seu e-mail',
+                                onChangeText: setEmail,
+                                keyboardType: 'email-address',
+                            }}
+                            formProps={{ name: 'email', control }} />
                         <Input
                             icon='lock'
-                            inputProps={{ placeholder: 'Digite sua senha', onChangeText: setPassword, secureTextEntry: true }}
-                            formProps={{ name: 'password', control }}
-                            error={error}
-                        // onChangeText={(password) => setPassword(password)} 
-                        // label='Senha' 
-                        // type='password' 
-                        // placeholder='Digite sua senha' 
-                        // color={colors.white} 
-                        // secureTextEntry={true} 
-                        />
+                            inputProps={{
+                                placeholder: 'Digite sua senha',
+                                onChangeText: setPassword,
+                                secureTextEntry: true
+                            }}
+                            formProps={{ name: 'password', control }} />
                         <PrimaryButton onPress={onSubmit} title="ENTRAR" />
                     </View>
-
-                    {error ? <MessageToast message={error} type='error' /> : success ? <MessageToast message={success} type='success' /> : null}
                 </View>
             </ScrollView>
         </KeyboardAvoidingView>
@@ -81,11 +81,13 @@ const styles = StyleSheet.create({
     },
     inputsView: {
         gap: 10,
+        paddingTop: 16
     },
     logoView: {
         display: 'flex',
         alignItems: 'center',
-        position: 'relative'
+        position: 'relative',
+        paddingTop: 24,
     },
     image: {
         width: 250,
