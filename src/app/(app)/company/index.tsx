ed9524f023439af16534144509af1a06861313a3
Mayara <maya.spieker@gmail.com>
2025-01-17 12:06:11 -0300

    feat: redirecionamento do card pros pontos de coleta

--- src/app/(app)/company/index.tsx
+++ src/app/(app)/company/index.tsx
@@ -1,10 +1,42 @@
+import { Card } from "@/components/card";
 import { MainLayout } from "@/components/main-layout";
-import { Text } from "react-native";
+import { globalsStyles } from "@/utils/globals-styles";
+import { RelativePathString, router } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Home(){
+function redirectTo(path: string) {
+    router.push(path as RelativePathString);
+}
+
+export default function Home() {
     return (
         <MainLayout>
-            <Text>opa</Text>
+            <View style={styles.carroussel}>
+                <Text style={globalsStyles.title}>Pontos de Coleta</Text>
+                <View style={styles.cardContainer}>
+                    <Card text="Cadastrar Ponto de Coleta" type="ponto-coleta" />
+                    <Card text="Ver meus Pontos de Coleta" type="info" onPress={() => router.push('/(app)/company/manage-collect-points')} />
+                </View>
+            </View>
+
+            <View style={styles.carroussel}>
+                <Text style={globalsStyles.title}>Resíduos</Text>
+                <View style={styles.cardContainer}>
+                    <Card text="Cadastrar Resíduo" icon="plus-circle" />
+                    <Card text="Ver resíduos cadastrados" type="info" />
+                </View>
+            </View>
         </MainLayout>
     )
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 12
+    },
+    carroussel: {
+        gap: 12
+    }
+})
