cc9d088419900276e2eac7ca32888bd6d049e6ab
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2025-01-19 17:18:52 -0300

    feat: fetch e delecao dos pontos de coleta

--- api/hooks/useCollectPoint.ts
+++ api/hooks/useCollectPoint.ts
@@ -1,15 +1,35 @@
 import { CollectPointType } from "@/utils/types";
 import { CollectPointService } from "api/services/CollectPointService";
+import { useEffect, useState } from "react";
 
 export function useCollectPointRegister() {
   const service = new CollectPointService();
+  const [collectPoints, setCollectPoints] = useState<CollectPointType[]>([]);
 
   const registerCollectPoint = async (data: CollectPointType) => {
     const response = await service.createCollectPoint(data);
     return response;
   }
 
+  const fetchCollectPoints = async () => {
+    const response = await service.fetchCollectPoints();
+    setCollectPoints(response.body);
+  }
+
+  const deleteCollectPoint = async (pointUUID: string) => {
+    const response = await service.deleteCollectPoint(pointUUID);
+    console.log(response);
+    return response;
+  }
+
+  useEffect(() => {
+    fetchCollectPoints();
+  })
+
   return {
-    registerCollectPoint
+    registerCollectPoint, 
+    collectPoints,
+    deleteCollectPoint,
+    fetchCollectPoints
   }
 }
