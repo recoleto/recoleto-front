c4be075d1a3f61f6354686f46b75ce553d7ca264
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2025-02-04 15:50:42 -0300

    styes: sem notif texto

--- src/app/(app)/user/notification.tsx
+++ src/app/(app)/user/notification.tsx
@@ -3,22 +3,23 @@ import { NotifCard } from "@/components/notif-card";
 import { MainLayout } from "@/components/main-layout";
 import { useGetNotification } from "api/hooks/useGetNotification";
 import { globalsStyles } from "@/globals-styles";
+import { colors, font } from "@/utils/globals";
 
 export default function Notifications() {
   const { notifications } = useGetNotification();
-  
+
   return (
     <MainLayout>
       <View style={styles.notifContainer}>
         <Text style={globalsStyles.title}>Suas notificações</Text>
         <Text style={globalsStyles.text}>Aqui você pode ver todas as notificações dos seus descartes.</Text>
-        {notifications.map((notif, index) => 
-        <NotifCard key={index} 
-        createdAt={notif.createdAt}
-        requestNumber={notif.requestNumber}
-        status={notif.status}
-        userName={notif.userName}
-        points={notif.points} />)}
+        {notifications && notifications.length > 0 ? notifications.map((notif, index) =>
+          <NotifCard key={index}
+            createdAt={notif.createdAt}
+            requestNumber={notif.requestNumber}
+            status={notif.status}
+            userName={notif.userName}
+            points={notif.points} />) : <Text style={styles.noNotifText}>Você não tem notificações.</Text>}
       </View>
     </MainLayout>
   )
@@ -29,5 +30,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     gap: 12
+  },
+  noNotifText: {
+    fontSize: font.size.medium,
+    color: colors.grey300,
+    textAlign: 'center'
   }
 })
