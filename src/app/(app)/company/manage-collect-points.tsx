60aa6ba13940d53c6ae447c616cadf3620300b1f
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2025-02-04 14:19:44 -0300

    feat: refresh das info com refresh control

--- src/app/(app)/company/manage-collect-points.tsx
+++ src/app/(app)/company/manage-collect-points.tsx
@@ -1,30 +1,46 @@
 import { ProfileLayout } from "@/components/profile-layout";
 import { ScrollView } from "react-native-gesture-handler";
 import { globalsStyles } from "../../../utils/globals-styles";
-import { Text } from "react-native";
+import { RefreshControl, Text } from "react-native";
 import { CollectPointCard } from "@/components/collect-point-card";
 import { useCollectPointCompany } from "api/hooks/useCollectPointCompany";
+import { useCallback, useState } from "react";
 
 export default function ManageCollectPointsScreen() {
-  const { collectPoints } = useCollectPointCompany();
+  const { collectPoints, fetchCollectPoints } = useCollectPointCompany();
+  const [refreshing, setRefreshing] = useState(false);
 
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchCollectPoints(); // Recarrega os pontos de coleta
+    setRefreshing(false);
+  }, []);
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
       <ProfileLayout>
         <Text style={globalsStyles.title}>Pontos de Coleta:</Text>
-        <Text> Aqui você pode visualizar e gerenciar todos os seus pontos de coleta</Text>
-        {collectPoints && collectPoints.length > 0 ? collectPoints.map((point) => (
-          <CollectPointCard
-            key={point.pointUUID}
-            pointUUID={point.pointUUID}
-            name={point.name}
-            street={point.street}
-            number={point.number}
-            cep={point.cep}
-            urbanSolidWaste={point.urbanSolidWaste}
-            phone={point.phone}
-          />
-        )) : <Text>Nenhum ponto de coleta cadastrado.</Text>}
+        <Text>Aqui você pode visualizar e gerenciar todos os seus pontos de coleta</Text>
+
+        {collectPoints && collectPoints.length > 0 ? (
+          collectPoints.map((point) => (
+            <CollectPointCard
+              key={point.pointUUID}
+              pointUUID={point.pointUUID}
+              name={point.name}
+              street={point.street}
+              number={point.number}
+              cep={point.cep}
+              urbanSolidWaste={point.urbanSolidWaste}
+              phone={point.phone}
+            />
+          ))
+        ) : (
+          <Text>Nenhum ponto de coleta cadastrado.</Text>
+        )}
       </ProfileLayout>
     </ScrollView>
   );
