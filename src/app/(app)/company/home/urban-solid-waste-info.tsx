dfc2a17e1b7a3c89cb0a72d01aa441673c58a64c
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2025-02-04 13:21:02 -0300

    feat: pesquisa no rsu completa

--- src/app/(app)/company/home/urban-solid-waste-info.tsx
+++ src/app/(app)/company/home/urban-solid-waste-info.tsx
@@ -1,48 +1,100 @@
+import { useState } from "react";
 import { globalsStyles } from "@/globals-styles";
 import { formatUrbanSolidWasteCategory } from "@/utils/utils";
 import { useUrbanSolidWaste } from "api/hooks/useUrbanSolidWaste";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 import { View, Text, SectionList } from "react-native";
+import { Input } from "@/components/input";
+import { useForm } from "react-hook-form";
+import { colors, font } from "@/utils/globals";
 
 export default function UrbanSolidWasteCategoryInfo() {
-    const { groupedUrbanSolidWastes } = useUrbanSolidWaste();
-    return (
-        <View style={styles.container}>
-            <Text style={globalsStyles.titlePrimary}>RESÍDUOS SÓLIDOS URBANOS CADASTRADOS</Text>
-            <SectionList
-                sections={groupedUrbanSolidWastes}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <View style={styles.wasteView}>
-                    <Text>Resíduo: {item.name}</Text>
-                    <Text>Pontos: {item.points}</Text>
-                    <Text>Categoria: {formatUrbanSolidWasteCategory(item.type)}</Text>
-                </View>}
-                renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-            />
-        </View>
+  const { control } = useForm();
+  const { groupedUrbanSolidWastes } = useUrbanSolidWaste();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredSections = groupedUrbanSolidWastes.map(section => ({
+    ...section,
+    data: section.data.filter(item =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
     )
+  })).filter(section => section.data.length > 0);
+
+  return (
+    <View style={styles.container}>
+      <Text style={globalsStyles.titlePrimary}>RESÍDUOS SÓLIDOS URBANOS CADASTRADOS</Text>
+      {/* <TextInput
+                style={styles.searchBar}
+                placeholder="Pesquisar RSU..."
+                value={searchQuery}
+                onChangeText={setSearchQuery}
+            /> */}
+      <Input
+        icon="search"
+        formProps={{
+          control,
+          rules: {
+            required: false
+          },
+          name: "searchQuery",
+        }}
+        inputProps={{
+          onChangeText(text) {
+            setSearchQuery(text);
+          },
+        }} />
+      <SectionList
+        sections={filteredSections}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.wasteView}>
+            <Text style={[globalsStyles.text, styles.wasteText]}>Resíduo: {item.name}</Text>
+            <Text style={[globalsStyles.text, styles.wasteText]}>Pontos: {item.points}</Text>
+            <Text style={[globalsStyles.text, styles.wasteText]}>Categoria: {formatUrbanSolidWasteCategory(item.type)}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 42
-    },
-    sectionHeader: {
-        paddingTop: 2,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2,
-        fontSize: 14,
-        fontWeight: 'bold',
-        backgroundColor: 'rgba(247,247,247,1.0)',
-        textTransform: 'uppercase'
-    },
-    wasteView: {
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 12,
-        borderBottomWidth: 1,
-        borderRightWidth: 1,
-        borderBottomColor: 'lightgray'
-    }
-})
+  container: {
+    paddingTop: 42,
+    paddingHorizontal: 24,
+    gap: 10
+  },
+  searchBar: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 10,
+    paddingLeft: 10
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: font.size.mediumX,
+    fontFamily: font.family.bold,
+    backgroundColor: colors.greenPrimary,
+    textTransform: "uppercase",
+    color: colors.white,
+  },
+  wasteView: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 12,
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderBottomColor: "lightgray"
+  },
+  wasteText: {
+    fontSize: font.size.medium,
+  }
+});
